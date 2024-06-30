40047922d3dcef6f20018ccf946065cdd383518e
Chirag-Jani <chirag.jani59001@gmail.com>
2024-06-30 23:45:34 +0530

    feat: .env added and API call done

--- src/pages/ContactUs/ContactForm.jsx
+++ src/pages/ContactUs/ContactForm.jsx
@@ -1,6 +1,59 @@
-import Button from "../../components/global/Button";
+import { useState } from "react";
+import axios from "axios";
 
 const ContactForm = () => {
+  const [userinput, setUserinput] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    country: "",
+    number: "",
+    message: "",
+  });
+
+  // eslint-disable-next-line no-unused-vars
+  const submitForm = async (e) => {
+    e.preventDefault();
+
+    // eslint-disable-next-line no-unused-vars
+    const emailText = `
+  Hello,
+
+  Here are the details provided by the user:
+
+  First Name: ${userinput.firstname}
+  Last Name: ${userinput.lastname}
+  Email: ${userinput.email}
+  Country: ${userinput.country}
+  Phone Number: ${userinput.number}
+
+  Message:
+  ${userinput.message}
+
+  Best regards,
+`;
+    let res = axios
+      // eslint-disable-next-line no-undef
+      .post(`${process.env.NEXT_PUBLIC_SERVER_URL}/send-mail`, {
+        emailText,
+      })
+      .then((data) => {
+        console.log(data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    console.log("Email Sent: ", res);
+  };
+
+  const handleUserInput = (e) => {
+    setUserinput({
+      ...userinput,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <div className=" px-6 py-5 lg:px-12">
       <div className="pb-10">
@@ -23,10 +76,12 @@ const ContactForm = () => {
             <div className="mt-2.5">
               <input
                 type="text"
-                name="first-name"
+                name="firstname"
                 id="first-name"
                 autoComplete="given-name"
                 className="block w-full rounded-md border-0 px-3.5 outline-none py-2 text-[#101013] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#6254ff] sm:text-sm sm:leading-6"
+                onChange={handleUserInput}
+                value={userinput.firstname}
               />
             </div>
           </div>
@@ -40,10 +95,12 @@ const ContactForm = () => {
             <div className="mt-2.5">
               <input
                 type="text"
-                name="last-name"
-                id="last-name"
+                name="lastname"
+                id="lastname"
                 autoComplete="family-name"
                 className="block w-full rounded-md border-0 px-3.5 outline-none py-2 text-[#101013] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset  focus:ring-[#6254ff] sm:text-sm sm:leading-6"
+                onChange={handleUserInput}
+                value={userinput.lastname}
               />
             </div>
           </div>
@@ -62,6 +119,8 @@ const ContactForm = () => {
                 id="email"
                 autoComplete="email"
                 className="block w-full rounded-md border-0 px-3.5 outline-none py-2 text-[#101013] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#6254ff] sm:text-sm sm:leading-6"
+                onChange={handleUserInput}
+                value={userinput.email}
               />
             </div>
           </div>
@@ -81,6 +140,8 @@ const ContactForm = () => {
                   id="country"
                   name="country"
                   className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:none sm:text-sm"
+                  onChange={handleUserInput}
+                  value={userinput.country}
                 >
                   <option>IN</option>
                   <option>US</option>
@@ -90,10 +151,12 @@ const ContactForm = () => {
               </div>
               <input
                 type="tel"
-                name="phone-number"
+                name="number"
                 id="phone-number"
                 autoComplete="tel"
                 className="block w-full rounded-md border-0  px-3.5 outline-none py-2 pl-24 text-[#101013] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#6254ff] sm:text-sm sm:leading-6"
+                onChange={handleUserInput}
+                value={userinput.number}
               />
             </div>
           </div>
@@ -111,12 +174,24 @@ const ContactForm = () => {
                 rows={4}
                 className="block w-full rounded-md border-0 px-3.5 outline-none py-2 text-[#101013] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#6254ff] sm:text-sm sm:leading-6"
                 defaultValue={""}
+                onChange={handleUserInput}
+                value={userinput.message}
               />
             </div>
           </div>
         </div>
         <div className="mt-10 flex justify-center">
-          <Button name={"send message"} />
+          <button
+            className=""
+            style={{
+              border: "1px solid",
+              borderRadius: "10px",
+              padding: "10px",
+            }}
+            onClick={submitForm}
+          >
+            Send Message
+          </button>
         </div>
       </form>
     </div>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    define: {
+      "process.env.NEXT_PUBLIC_SERVER_URL": JSON.stringify(
+        env.NEXT_PUBLIC_SERVER_URL
+      ),
+    },
+    plugins: [react()],
+  };
+});
